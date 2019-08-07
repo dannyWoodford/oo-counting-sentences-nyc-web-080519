0c751b9ac90799677bbe0bc0e9e2fb08916657fb
daniel Woodford <daniel.woodford.g@gmail.com>
2019-08-07 11:35:23 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
-require 'pry'
+irequire 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
-
+ 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    splited = self.split("."||"?"||"!")
+    splited.count{ |punct| punct == "."||"?"||"!"} 
+    
   end
 end
